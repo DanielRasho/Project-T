8bd263af93b6b887d5b3c67eff1997243e6fd56c
DanielRasho <ray22933@uvg.edu.gt>
2022-05-03 17:42:46 -0600

    Add setup.py as a script to verify if dependencies at requirements.txt are installed

---
+++ setup.py
@@ -0,0 +1,16 @@
+import pkg_resources    # Functionalities to check project requirements.
+from pkg_resources import DistributionNotFound
+
+"""
+This script checks if the user has all dependencies installed. 
+    If not, it just stop the execution.
+"""
+dependencies = []
+with open ("requirements.txt", "r") as modules_depedencies:
+    for modules in modules_depedencies:
+        dependencies.append(modules.rstrip())
+try:
+    pkg_resources.require(dependencies)
+except DistributionNotFound:
+    print("\nMissing libreries, if you use pip, try running in this location:\n\n\tpython -m pip install -r requirements.txt")
+    exit()
