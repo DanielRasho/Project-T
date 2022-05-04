d5a5645f859c8fbd1fe3b6748006b308df450f3d
DanielRasho <ray22933@uvg.edu.gt>
2022-05-04 08:41:46 -0600

    Eliminando funciones desconocidas en main.py

--- main.py
+++ main.py
@@ -2,18 +2,6 @@ import setup
 from modules.interface.menus import menu_builder, clean_screen
 import validators;
 
-#Verificar que el valor sea cadena y tenga un carácter como mínimo
-def validate_string(value):
-    if not validators.length(value, min=1):
-        return False;
-    return True;
-
-#Verificar que el valor sea de tipo entero y sea 0 como mínimo
-def validate_int(value):
-    if not validators.between(value, min=0):
-        return False;   
-    return True;       
-
 accion =  menu_builder('Bienvenido a Proyecto T',
             ['Ingresar', 'Registrarse'],
             '',
