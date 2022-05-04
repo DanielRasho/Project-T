21032242820b43899bb07a17728850e26117a0b2
DanielRasho <ray22933@uvg.edu.gt>
2022-05-04 13:22:31 -0600

    requirements.txt actualizado con libreria pandas

--- modules/database/users.py
+++ modules/database/users.py
@@ -1,36 +1,37 @@
-﻿import pandas
+﻿import pandas as pd
+from enum import Enum
 
-PROPIEDADES_USUARIO = (
-    "TELEFONO", 
-    "CONTRASENA",
-    "GENERO",
-    "EDAD", 
-    "NOMBRE", 
-    "APELLIDO", 
-    "ETIQUETAS")
+class Campos_Usuario(Enum):
+    telefono = "TELEFONO";
+    contrasena = "CONTRASENA",
+    genero ="GENERO",
+    edad = "EDAD", 
+    nombre = "NOMBRE", 
+    apellido = "APELLIDO", 
+    etiquetas = "ETIQUETAS"
 
 class Base_de_datos:
     ruta = ""
     def __init__(self, ruta):
         self.ruta = ruta
 
-def actualizar_campos(file_path):
-    pass
+    def actualizar_campos(self):
+        datos = pd.read_csv()
 
-def crea_usuario():
-    pass
+    def crea_usuario():
+        pass
 
-def elimina_usuario():
-    pass
+    def elimina_usuario():
+        pass
 
-def buscar_usuario():
-    pass
+    def buscar_usuario():
+        pass
 
-def existe_usuario():
-    pass
+    def existe_usuario():
+        pass
 
-def obtener_propiedades_usuario():
-    pass
+    def obtener_propiedades_usuario():
+        pass
 
-def validar_usuario():
-    pass
+    def validar_usuario():
+        pass
