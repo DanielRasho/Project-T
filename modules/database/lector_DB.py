73a7bfcfa75176990fbd8caac5cf4971122c6383
DanielRasho <ray22933@uvg.edu.gt>
2022-05-27 20:52:24 -0600

    Create search function on Base_De_Datos class

--- modules/database/lector_DB.py
+++ modules/database/lector_DB.py
@@ -14,6 +14,7 @@ from .campos_DB import (
 # TIPOS
 Telefono = int
 
+
 class Lector_BD:
     RUTA: str = ""
     DATOS: pd.DataFrame = None
@@ -45,28 +46,34 @@ class Lector_BD:
         return self.DATOS.columns.tolist()
 
     def es_campo_valido(self, campo: str) -> None:
-         if campo not in self.CAMPOS:
+        if campo not in self.CAMPOS:
             raise ("columna_indice no existe en la base de datos actual.")
 
-    def existe_item(self, indice_buscado: str | int, columna_indice: str = None) -> bool:
+    def existe_item(
+        self, indice_buscado: str | int, columna_indice: str = None
+    ) -> bool:
         if columna_indice is not None:
             self.es_campo_valido(columna_indice)
             return (self.DATOS[columna_indice] == indice_buscado).any()
         else:
-            return (indice_buscado in self.DATOS.index)
+            return indice_buscado in self.DATOS.index
 
     def agregar_nuevo_item(
-        self, item_id: str | int, campos_item: dict, columna_indice: str = None  
+        self, item_id: str | int, campos_item: dict, columna_indice: str = None
     ) -> bool:
         if self.existe_item(item_id, columna_indice=columna_indice) == False:
 
-            if columna_indice is not None:          # Estableciendo indice si columna_indice especificado
+            if (
+                columna_indice is not None
+            ):  # Estableciendo indice si columna_indice especificado
                 self.es_campo_valido(columna_indice)
                 self.DATOS.set_index(columna_indice, inplace=True)
 
-            self.DATOS.loc[item_id] = campos_item   # Agregando el item
+            self.DATOS.loc[item_id] = campos_item  # Agregando el item
 
-            if columna_indice is not None:          # Reestableciendo indice si columna_indice especificado
+            if (
+                columna_indice is not None
+            ):  # Reestableciendo indice si columna_indice especificado
                 self.DATOS.reset_index(inplace=True)
 
             self.escribir_database()
@@ -158,7 +165,9 @@ class Lector_BD:
 
     def agregar_campo(self, nombre_campo: str, default_value: Any) -> None:
         if nombre_campo not in self.CAMPOS:
-            self.DATOS.insert(len(self.DATOS.columns), column=nombre_campo, value=default_value)
+            self.DATOS.insert(
+                len(self.DATOS.columns), column=nombre_campo, value=default_value
+            )
             self.escribir_database()
         else:
             raise ("Esa columna ya existe en el registro")
@@ -176,8 +185,8 @@ class Lector_BD:
             self.escribir_database()
         else:
             raise ("Esa columna no existe en el registro")
-    
-    def ordenar_dataframe(self, campos, ascendente = True) -> None:
+
+    def ordenar_dataframe(self, campos, ascendente=True) -> None:
         self.DATOS.sort_values(by=campos, ascending=ascendente)
         self.escribir_database()
 
@@ -188,6 +197,7 @@ class Lector_BD:
     def obtener_dataframe(self) -> pd.DataFrame:
         return self.DATOS
 
+
 class BD_usuarios:
     LECTOR_DB_USUARIOS: Lector_BD = None
 
@@ -195,14 +205,22 @@ class BD_usuarios:
         self.LECTOR_DB_USUARIOS = Lector_BD(ruta, nombre_tabla_usuarios)
 
     def existe_usuario(self, telefono: int) -> bool:
-        return self.LECTOR_DB_USUARIOS.existe_item(telefono, columna_indice=Campos_Usuario.TELEFONO)
-
-    def crear_usuario(self, usuario: Usuario) -> bool:
-        return self.LECTOR_DB_USUARIOS.agregar_nuevo_item(
-            usuario.TELEFONO, usuario.user_as_dict(), columna_indice= Campos_Usuario.TELEFONO
+        return self.LECTOR_DB_USUARIOS.existe_item(
+            telefono, columna_indice=Campos_Usuario.TELEFONO
         )
 
-    def eliminar_usuario(self, telefono:int) -> bool:
+    def crear_usuario(self, usuario: Usuario) -> bool:
+        if usuario is None:
+            raise (
+                "El parametro usuario es tipo None. Intruzca un objeto de la clase Usuario."
+            )
+        return self.LECTOR_DB_USUARIOS.agregar_nuevo_item(
+            usuario.TELEFONO,
+            usuario.user_as_dict(),
+            columna_indice=Campos_Usuario.TELEFONO,
+        )
+
+    def eliminar_usuario(self, telefono: int) -> bool:
         return self.LECTOR_DB_USUARIOS.eliminar_item(telefono, Campos_Usuario.TELEFONO)
 
     def validar_usuario(self, telefono: int, contrasena: str) -> bool:
@@ -218,13 +236,27 @@ class BD_usuarios:
             return False
 
     def editar_usuario(self, usuario: Usuario):
-        nuevos_valores = usuario.user_as_dict(campos_ignorados=[Campos_Usuario.TELEFONO, Campos_Usuario.CONTACTOS, Campos_Usuario.OCUPACIONES]).items()
+        if usuario is None:
+            raise (
+                "El parametro usuario es tipo None. Intruzca un objeto de la clase Usuario."
+            )
+        nuevos_valores = usuario.user_as_dict(
+            campos_ignorados=[
+                Campos_Usuario.TELEFONO,
+                Campos_Usuario.CONTACTOS,
+                Campos_Usuario.OCUPACIONES,
+            ]
+        ).items()
         for campo, valor in nuevos_valores:
             self.LECTOR_DB_USUARIOS.editar_item(
                 usuario.TELEFONO, campo, valor, columna_indice=Campos_Usuario.TELEFONO
             )
 
     def editar_telefono_usuario(self, usuario: Usuario, nuevo_telefono):
+        if usuario is None:
+            raise (
+                "El parametro usuario es tipo None. Intruzca un objeto de la clase Usuario."
+            )
         self.LECTOR_DB_USUARIOS.editar_item_id(
             usuario.TELEFONO, Campos_Usuario.TELEFONO, nuevo_telefono
         )
@@ -242,7 +274,7 @@ class BD_contactos:
     def __init__(self, ruta: str, tabla_contactos: Nombres_Tablas):
         self.LECTOR_DB_CONTACTO = Lector_BD(ruta, tabla_contactos)
 
-    def obtener_contactos_usuario(self, telefono_usuario:int) -> list[Telefono]:
+    def obtener_contactos_usuario(self, telefono_usuario: int) -> list[Telefono]:
         contactos = self.LECTOR_DB_CONTACTO.buscar_items(
             Campos_Contactos.TELEFONO, [telefono_usuario], metodo="exacto"
         )
@@ -252,46 +284,54 @@ class BD_contactos:
         return telefonos_contactos
 
     def existe_contacto(self, telefono_usuario, telefono_contacto: int) -> bool:
-        return (telefono_contacto in self.obtener_contactos_usuario(telefono_usuario))
+        return telefono_contacto in self.obtener_contactos_usuario(telefono_usuario)
 
     def agregar_contacto(self, usuario: Usuario, telefono_contacto: int) -> bool:
+        if usuario is None:
+            raise (
+                "El parametro usuario es tipo None. Intruzca un objeto de la clase Usuario."
+            )
         if usuario.TELEFONO == telefono_contacto:
-            raise("El telefono del usuario y el contacto, no pueden ser iguales.")
+            raise ("El telefono del usuario y el contacto, no pueden ser iguales.")
         if self.existe_contacto(usuario.TELEFONO, telefono_contacto) == True:
             return False
         new_index = len(self.LECTOR_DB_CONTACTO.obtener_dataframe().index)
         hubo_exito = self.LECTOR_DB_CONTACTO.agregar_nuevo_item(
             new_index,
-            {Campos_Contactos.TELEFONO: usuario.TELEFONO,
-            Campos_Contactos.CONTACTOS: telefono_contacto},
-            columna_indice=None
+            {
+                Campos_Contactos.TELEFONO: usuario.TELEFONO,
+                Campos_Contactos.CONTACTOS: telefono_contacto,
+            },
+            columna_indice=None,
         )
 
         if hubo_exito == True:
             self.LECTOR_DB_CONTACTO.ordenar_dataframe([Campos_Contactos.TELEFONO])
             return True
-        else: 
+        else:
             return False
 
     def eliminar_contacto(self, telefono_usuario, telefono_contacto: int) -> bool:
-        if Usuario.TELEFONO == telefono_contacto:
-            raise("El telefono del usuario y el contacto, no pueden ser iguales.")
+        if telefono_usuario == telefono_contacto:
+            raise ("El telefono del usuario y el contacto, no pueden ser iguales.")
         elif self.existe_contacto(telefono_usuario, telefono_contacto) == True:
             datos = self.LECTOR_DB_CONTACTO.obtener_dataframe()
             contactos_index = datos.index[
-                (datos[Campos_Contactos.TELEFONO] == telefono_usuario) & 
-                (datos[Campos_Contactos.CONTACTOS] == telefono_contacto)].to_list()
+                (datos[Campos_Contactos.TELEFONO] == telefono_usuario)
+                & (datos[Campos_Contactos.CONTACTOS] == telefono_contacto)
+            ].to_list()
             if len(contactos_index) != 0:
                 for contacto in contactos_index:
                     self.LECTOR_DB_CONTACTO.eliminar_item(contacto)
                     return True
         return False
 
-
     def obtener_dataframe(self):
         return self.LECTOR_DB_CONTACTO.obtener_dataframe()
 
+
 # ===========================================================
+
 
 class BD_ocupaciones:
     LECTOR_DB_OCUPACIONES: Lector_BD = None
@@ -299,7 +339,7 @@ class BD_ocupaciones:
     def __init__(self, ruta: str, nombre_tabla_usuarios: Nombres_Tablas):
         self.LECTOR_DB_OCUPACIONES = Lector_BD(ruta, nombre_tabla_usuarios)
 
-    def obtener_ocupaciones_usuario(self, telefono_usuario:int) -> list[str]:
+    def obtener_ocupaciones_usuario(self, telefono_usuario: int) -> list[str]:
         ocupaciones = self.LECTOR_DB_OCUPACIONES.buscar_items(
             Campos_Ocupacion.TELEFONO, [telefono_usuario], metodo="exacto"
         )
@@ -308,31 +348,42 @@ class BD_ocupaciones:
             ocupaciones_usuario.append(ocupacion[Campos_Ocupacion.OCUPACION])
         return ocupaciones_usuario
 
-    def existe_ocupacion(self, telefono_usuario, ocupacion_usuario:str ) -> bool:
-        return (ocupacion_usuario in self.obtener_ocupaciones_usuario(telefono_usuario))
+    def existe_ocupacion(self, telefono_usuario, ocupacion_usuario: str) -> bool:
+        return ocupacion_usuario in self.obtener_ocupaciones_usuario(telefono_usuario)
 
-    def agregar_ocupacion(self, usuario:Usuario, ocupacion_usuario:str) -> bool:
+    def agregar_ocupacion(self, usuario: Usuario, ocupacion_usuario: str) -> bool:
+        if usuario is None:
+            raise (
+                "El parametro usuario es tipo None. Intruzca un objeto de la clase Usuario."
+            )
         if self.existe_ocupacion(usuario.TELEFONO, ocupacion_usuario) == True:
             return False
         new_index = len(self.LECTOR_DB_OCUPACIONES.obtener_dataframe().index)
         hubo_exito = self.LECTOR_DB_OCUPACIONES.agregar_nuevo_item(
-            new_index, 
-            {Campos_Ocupacion.TELEFONO:  usuario.TELEFONO,
-            Campos_Ocupacion.OCUPACION: ocupacion_usuario},
-            columna_indice=None
+            new_index,
+            {
+                Campos_Ocupacion.TELEFONO: usuario.TELEFONO,
+                Campos_Ocupacion.OCUPACION: ocupacion_usuario,
+            },
+            columna_indice=None,
         )
         if hubo_exito == True:
             self.LECTOR_DB_OCUPACIONES.ordenar_dataframe([Campos_Contactos.TELEFONO])
             return True
-        else: 
+        else:
             return False
 
-    def eliminar_ocupacion(self, usuario, ocupacion_usuario: str) -> bool:
+    def eliminar_ocupacion(self, usuario: Usuario, ocupacion_usuario: str) -> bool:
+        if usuario is None:
+            raise (
+                "El parametro usuario es tipo None. Intruzca un objeto de la clase Usuario."
+            )
         if self.existe_contacto(usuario, ocupacion_usuario) == True:
             datos = self.LECTOR_DB_OCUPACIONES.obtener_dataframe()
             ocupaciones_index = datos.index[
-                (datos[Campos_Ocupacion.TELEFONO] == usuario.TELEFONO) & 
-                (datos[Campos_Ocupacion.OCUPACION] == ocupacion_usuario)].to_list()
+                (datos[Campos_Ocupacion.TELEFONO] == usuario.TELEFONO)
+                & (datos[Campos_Ocupacion.OCUPACION] == ocupacion_usuario)
+            ].to_list()
             if len(ocupaciones_index) != 0:
                 for ocupacion in ocupaciones_index:
                     self.LECTOR_DB_OCUPACIONES.eliminar_item(ocupacion)
@@ -341,15 +392,21 @@ class BD_ocupaciones:
 
     def obtener_dataframe(self):
         return self.LECTOR_DB_OCUPACIONES.obtener_dataframe()
-    
-class Base_De_Datos(BD_usuarios, BD_contactos, BD_ocupaciones):
 
-    def __init__(self, ruta:str, nombre_tabla_usuarios, nombre_tabla_contactos, nombre_tabla_ocupaciones) -> None:
+
+class Base_De_Datos(BD_usuarios, BD_contactos, BD_ocupaciones):
+    def __init__(
+        self,
+        ruta: str,
+        nombre_tabla_usuarios,
+        nombre_tabla_contactos,
+        nombre_tabla_ocupaciones,
+    ) -> None:
         BD_usuarios.__init__(self, ruta, nombre_tabla_usuarios)
         BD_contactos.__init__(self, ruta, nombre_tabla_contactos)
         BD_ocupaciones.__init__(self, ruta, nombre_tabla_ocupaciones)
-    
-    def obtener_usuario(self, telefono:int, contrasena:str) -> Usuario:
+
+    def obtener_usuario(self, telefono: int, contrasena: str) -> Usuario:
         if self.validar_usuario(telefono, contrasena) == True:
             campos_usuario = self.obtener_propiedades_usuario(telefono)
             ocupaciones_usuario = self.obtener_ocupaciones_usuario(telefono)
@@ -359,7 +416,14 @@ class Base_De_Datos(BD_usuarios, BD_contactos, BD_ocupaciones):
             usuario.establecer_ocupaciones(ocupaciones_usuario)
             return usuario
 
-    def registrar_usuario(self, usuario:Usuario) -> bool:
+    def registrar_usuario(self, usuario: Usuario) -> bool:
+        a = True
+        b = True
+        c = True
+        if usuario is None:
+            raise (
+                "El parametro usuario es tipo None. Intruzca un objeto de la clase Usuario."
+            )
         if self.validar_usuario(usuario.TELEFONO, usuario.CONTRASENA) == False:
             a = self.crear_usuario(usuario)
             for contacto in usuario.CONTACTOS:
@@ -367,46 +431,69 @@ class Base_De_Datos(BD_usuarios, BD_contactos, BD_ocupaciones):
             for ocupacion in usuario.OCUPACIONES:
                 c = self.agregar_ocupacion(usuario, ocupacion)
             if a == False:
-                raise("Hubo un error al crear el usuario.")
+                raise ("Hubo un error al crear el usuario.")
             elif b == False:
-                raise("Hubor un error al agregar los contactos")
+                raise ("Hubor un error al agregar los contactos")
             elif c == False:
-                raise("Hubo un error al agregar las ocupaciones")
+                raise ("Hubo un error al agregar las ocupaciones")
             else:
                 return True
         return False
-    
 
-    # PENDIENTE POR PROBAR
-    ''''
-    def eliminar_cuenta_usuario(self, telefono_usuario, contrasena:str) -> bool:
+    def eliminar_cuenta_usuario(self, telefono_usuario, contrasena: str) -> bool:
+        a = True
+        b = True
+        c = True
+        usuario = self.obtener_usuario(telefono_usuario, contrasena)
+        if usuario is None:
+            raise (
+                "El telefono y contrasena dados no corresponde con ningun usuario registrado."
+            )
+        a = self.eliminar_usuario(usuario.TELEFONO)
+        for contacto in usuario.CONTACTOS:
+            b = self.eliminar_contacto(usuario.TELEFONO, contacto)
+        for ocupacion in usuario.OCUPACIONES:
+            c = self.eliminar_ocupacion(usuario, ocupacion)
+        if a == False:
+            raise ("Hubo un error al eliminar el usuario.")
+        elif b == False:
+            raise ("Hubor un error al eliminar los contactos")
+        elif c == False:
+            raise ("Hubo un error al eliminar las ocupaciones")
+        else:
+            return True
 
-        if self.validar_usuario(usuario.TELEFONO) == True:
-            a = self.eliminar_usuario(usuario)
-            for contacto in usuario.CONTACTOS:
-                b = self.eliminar_contacto(usuario.TELEFONO, contacto)
-            for ocupacion in usuario.OCUPACIONES:
-                c = self.eliminar_ocupacion(usuario, ocupacion)
-            if a == False:
-                raise("Hubo un error al eliminar el usuario.")
-            elif b == False:
-                raise("Hubor un error al eliminar los contactos")
-            elif c == False:
-                raise("Hubo un error al eliminar las ocupaciones")
-            else:
-                return True
-        return False
-    '''
+    def busqueda_recursiva_por_ocupacion(
+        self, usuario: Usuario, ocupacion: str
+    ) -> list[Telefono]:
+        if usuario is None:
+            raise (
+                "El parametro usuario es tipo None. Intruzca un objeto de la clase Usuario."
+            )
+        posibles_telefonos = set()
+        coincidencias_finales = []
+        contactos_usuario = self.obtener_contactos_usuario(usuario.TELEFONO)
+        posibles_telefonos.update(contactos_usuario)
+        for contacto in contactos_usuario:
+            temp_contactos = set(self.obtener_contactos_usuario(contacto))
+            posibles_telefonos.union(temp_contactos)
+        posibles_telefonos.update(contactos_usuario)
 
+        for telefono in posibles_telefonos:
+            if self.existe_ocupacion(telefono, ocupacion) == True:
+                coincidencias_finales.append(telefono)
 
-    def busqueda_recursiva_por_ocupacion(self, usuario:Usuario, ocupacion:str) -> list[int]:
-        # DEVUELVE LISTA DE TELEFONOS
-        pass
+        return coincidencias_finales
 
     def mostrar_tablas(self) -> None:
         print(self.LECTOR_DB_USUARIOS.obtener_dataframe())
         print(self.LECTOR_DB_OCUPACIONES.obtener_dataframe())
         print(self.LECTOR_DB_CONTACTO.obtener_dataframe())
+
+    def limpiar_tablas(self) -> None:
+        self.LECTOR_DB_USUARIOS.limpiar_dataframe()
+        self.LECTOR_DB_CONTACTO.limpiar_dataframe()
+        self.LECTOR_DB_OCUPACIONES.limpiar_dataframe()
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -12,19 +12,16 @@ from modules.database.objects_DB import Usuario
 path = "./data/database.db"
 uwu = Lector_BD(path, Nombres_Tablas.OCUPACIONES)
 
+lector = Base_De_Datos(path, Nombres_Tablas.USUARIOS, Nombres_Tablas.CONTACTOS, Nombres_Tablas.OCUPACIONES)
 
-'''
+usuario = Usuario(543, "boichi", "Hombre", 23, "Carolyn", "Roldan", True, False)
+usuario.establecer_contactos([52, 51, 53])
+usuario.establecer_ocupaciones(["carpintero", "pasteles"])
 
-database = Base_De_Datos(path, Nombres_Tablas.USUARIOS, Nombres_Tablas.CONTACTOS, Nombres_Tablas.OCUPACIONES)
+lector.agregar_contacto(usuario, 533)
 
-marcelo = Usuario(54387323, "marcelo", "HOMBRE", 18, "Marcelo2", "Ruiz", True, False)
+#lector.registrar_usuario(usuario)
 
-marcelo.establecer_contactos([45674545])
+print(lector.busqueda_recursiva_por_ocupacion(usuario, "carpintero"))
 
-marcelo.establecer_ocupaciones(["Dibujante", "papel"])
-
-database.eliminar_cuenta_usuario(marcelo)
-
-database.mostrar_tablas()
-
-'''
+lector.mostrar_tablas()
