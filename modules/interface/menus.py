dea0dd06a36ac1f5769f15892ca2cbf26b298713
DanielRasho <ray22933@uvg.edu.gt>
2022-05-29 18:19:59 -0600

    Create functions for user interface

--- modules/interface/menus.py
+++ modules/interface/menus.py
@@ -1,52 +1,133 @@
-from readchar import readkey, key  
-import os
+import curses
+from curses import wrapper
+from curses.textpad import Textbox, rectangle
+from typing import List
 
+def print_centered(window: curses.window, text:str, optional_message:str = ""):
+    height = text.count("\n")
+    width = 0
+    for row in text.split("\n"):
+        if len(row) > width:
+            width = len(row)
+    y, x = window.getmaxyx()
+    y_banner = y//2 - height//2
+    x_banner = x//2 - width//2
+    window.clear()
     
-def menu_builder(header: str, options: list, footer: str, return_value = False, default_options = []):
-    cursor_position = 0
-    options += default_options
+    rectangle(window, y_banner, x_banner-2, y_banner+height, x_banner+width+2)
+    window.addstr(y_banner-2, x//2-len(optional_message)//2, optional_message)
+    for row in text.split("\n"):
+        window.addstr(y_banner, x_banner, row)
+        y_banner += 1
+    window.refresh()
 
+def centered_coords(window: curses.window, width: int, height: int):
+    h, w = window.getmaxyx()
+    x = w // 2 - width // 2
+    y = h // 2 - height // 2
+    return (x, y)
+
+def centered_box(window: curses.window, width: int, height: int):
+    x, y = centered_coords(window, width, height)
+    box = curses.newwin(height, width, y, x)
+    rectangle(window, y - 1, x - 1, y + height, x + width)
+    window.refresh()
+    return box
+
+def textbox_builder(
+    window: curses.window, width: int, height: int, header: str, footer: str
+) -> str:
+    # BOXES CREATION
+    box = centered_box(window, width, height)
+    x_cord, y_cord = centered_coords(window, width, height)
+    h, w = box.getmaxyx()
+    sub_box = curses.newwin(1, width - 4, y_cord + height - 5, x_cord + 2)
+    textbox = Textbox(sub_box)
+
+    # DRAWING BOXES
+    rectangle(box, height - 6, 1, height - 4, width - 2)
+    box.addstr(0, w // 2 - len(header) // 2, header, curses.A_UNDERLINE)
+    box.addstr(h - 2, w // 2 - len(footer) // 2, footer, curses.color_pair(2))
+
+    # GETTING USER INPUT
+    box.refresh()
+    sub_box.refresh()
+    curses.curs_set(1)
+    textbox.edit()
+    curses.curs_set(0)
+    # CLEARING OUT
+    window.clear()
+
+    return textbox.gather().replace(" ", "")
+
+def alert_message(window: curses.window, width, height, title, content, ending):
+    box = centered_box(window, width, height)
+    y, x = box.getmaxyx()
+    # ADDING THE CONTENT TO THE BOX
+    box.addstr(0, x // 2 - len(title) // 2, title, curses.A_UNDERLINE)
+    box.addstr(2, 0, content)
+    box.addstr(y - 1, x // 2 - len(ending) // 2, ending, curses.color_pair(2))
+    box.refresh()
+    # ERASING THE ALERT_MESSAGE IF ENTER KEY IS PRESSED
     while True:
-        
-        clean_screen()
-        print(f"{header}\n")
-        for index, option in enumerate(options):
-            if index == cursor_position:
-                print(f"\t>> {option}")
+        key = box.getch()
+        if key == curses.KEY_ENTER or key in [10, 13]:
+            box.clear()
+            window.clear()
+            box.refresh()
+            del box
+            break
+
+def menu_builder(
+    window: curses.window,
+    width: int,
+    header: str,
+    options: List[str],
+    footer: str,
+    default_options: List[str] = [],
+) -> str:
+    options += default_options
+    height = len(options) + 5
+    box = centered_box(window, width, height)
+    selected_row = 0
+    while True:
+        # DRAW MENU
+        box.clear()
+        box.addstr(0, width // 2 - len(header) // 2, header, curses.A_UNDERLINE)
+        for row, value in enumerate(options, ):
+            if row == selected_row:
+                box.addstr(row+2, width // 2 - len(value) // 2, value, curses.color_pair(2))
             else:
-                print(f"\t   {option}")
-        print(f"\n{footer}")
+                box.addstr(row+2, width // 2 - len(value) // 2, value)
+        box.addstr(height - 1, width // 2 - len(footer) // 2, footer)
+        box.refresh()
 
-        
-        new_cursor_position = readkey()
-        if new_cursor_position == key.UP:         
-            cursor_position -= 1
-            cursor_position %= len(options)
-        elif new_cursor_position == key.DOWN:     
-            cursor_position += 1
-            cursor_position %= len(options)
-        elif new_cursor_position == key.ENTER:   
-            if return_value is True: return options[cursor_position]
-            else: return cursor_position
+        # CHANGE ROW
+        key = window.getch()
+        if key == curses.KEY_UP:         
+            selected_row -= 1
+            selected_row %= len(options)
+        elif key == curses.KEY_DOWN:     
+            selected_row += 1
+            selected_row %= len(options)
+        elif key == curses.KEY_ENTER or key in [10, 13]:   
+            window.clear()
+            return options[selected_row]
 
-def clean_screen():
-    # Cleans terminal screen  
-    if os.name == "posix":  # If program is executed in Linux or Mac
-        os.system("clear")
+def confirmation_box(window: curses.window, message:str) -> bool:
+    window.clear()
+    answer = menu_builder(window, 50, message, ["SI", "NO"], footer="Presiona <Enter> para confirmar.")
+    if answer == "SI":
+        return True
+    if answer == "NO":
+        return False
+
+
+def es_telefono_valido(telefono:str):
+    validacion = telefono.replace("\n", "").strip().isnumeric()
+    if validacion==False:
+        return False
+    elif len(telefono) != 8:
+        return False
     else:
-        os.system("cls")
-
-def datosANDvalidacion_numeros():
-    x=True
-    while x == True:
-        usuario=input('Ingrese su número de teléfono\n')
-        validacion=usuario.isnumeric()
-        if validacion==False:
-            print("No has ingresado un número válido.\nRecuerda ingresar únicamente números")
-        else:
-            lenn=len(usuario)
-            if lenn == 8:
-                break
-            else:
-                print("Debe haber 8 digitos")
-    return usuario
+        return True
--- test.py
+++ test.py
@@ -1,27 +1,273 @@
-import pandas as pd
-from modules.database.lector_DB import (
-    BD_contactos,
-    BD_ocupaciones,
-    Lector_BD,
-    BD_usuarios,
-    Base_De_Datos,
-)
-from modules.database.campos_DB import Nombres_Tablas
+import curses
+
+from sqlalchemy import true
+from modules.interface.menus import *
+from typing import Tuple
+from curses.textpad import rectangle
+from config.definitions import CARPETA_RAIZ
+from modules.database.lector_DB import Base_De_Datos, Lector_BD, Telefono
 from modules.database.objects_DB import Usuario
+from modules.database.campos_DB import (
+    Campos_Usuario,
+    Campos_Ocupacion,
+    Campos_Contactos,
+    Nombres_Tablas,
+)
 
-path = "./data/database.db"
-uwu = Lector_BD(path, Nombres_Tablas.OCUPACIONES)
+RUTA = CARPETA_RAIZ + "/data/database.db"
+ESCRITOR_DB = Base_De_Datos(
+    RUTA, Nombres_Tablas.USUARIOS, Nombres_Tablas.CONTACTOS, Nombres_Tablas.OCUPACIONES
+)
+ESCRITOR_DB.limpiar_tablas()
 
-lector = Base_De_Datos(path, Nombres_Tablas.USUARIOS, Nombres_Tablas.CONTACTOS, Nombres_Tablas.OCUPACIONES)
+banner = r"""
+ _______                                                           __           
+/       \                                                         /  |          
+$$$$$$$  | __    __   _______   _______   ______   _______    ____$$ |  ______  
+$$ |__$$ |/  |  /  | /       | /       | /      \ /       \  /    $$ | /      \ 
+$$    $$< $$ |  $$ |/$$$$$$$/ /$$$$$$$/  $$$$$$  |$$$$$$$  |/$$$$$$$ |/$$$$$$  |
+$$$$$$$  |$$ |  $$ |$$      \ $$ |       /    $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |
+$$ |__$$ |$$ \__$$ | $$$$$$  |$$ \_____ /$$$$$$$ |$$ |  $$ |$$ \__$$ |$$ \__$$ |
+$$    $$/ $$    $$/ /     $$/ $$       |$$    $$ |$$ |  $$ |$$    $$ |$$    $$/ 
+$$$$$$$/   $$$$$$/  $$$$$$$/   $$$$$$$/  $$$$$$$/ $$/   $$/  $$$$$$$/  $$$$$$/  
+                                                                                
+         ______   __                                __                    
+        /      \ /  |                              /  |                  
+        /$$$$$$  |$$ |____    ______   _____  ____  $$ |____    ______     
+        $$ |  $$/ $$      \  /      \ /     \/    \ $$      \  /      \   
+        $$ |      $$$$$$$  | $$$$$$  |$$$$$$ $$$$  |$$$$$$$  | $$$$$$  |    
+        $$ |   __ $$ |  $$ | /    $$ |$$ | $$ | $$ |$$ |  $$ | /    $$ |  
+        $$ \__/  |$$ |  $$ |/$$$$$$$ |$$ | $$ | $$ |$$ |__$$ |/$$$$$$$ |        
+        $$    $$/ $$ |  $$ |$$    $$ |$$ | $$ | $$ |$$    $$/ $$    $$ |        
+         $$$$$$/  $$/   $$/  $$$$$$$/ $$/  $$/  $$/ $$$$$$$/   $$$$$$$/         
+                                               
+"""
+telefono_usuario = None
+contrasena_usuario = None
 
-usuario = Usuario(543, "boichi", "Hombre", 23, "Carolyn", "Roldan", True, False)
-usuario.establecer_contactos([52, 51, 53])
-usuario.establecer_ocupaciones(["carpintero", "pasteles"])
 
-lector.agregar_contacto(usuario, 533)
+def login_page(screen) -> Tuple[int, str]:
+    telefono = None
+    contrasena = None
+    telefono_valido = False
+    continuar_verificacion = True
+    while continuar_verificacion == True:
+        screen.clear()
+        telefono = textbox_builder(screen, 50, 10, "INGRESA TU TELEFONO", "ENTER")
+        telefono_valido = es_telefono_valido(telefono)
+        screen.clear()
+        contrasena = textbox_builder(screen, 50, 10, "INGRESA TU CONTRASENA", "ENTER")
+        existe_usuario = ESCRITOR_DB.validar_usuario(int(telefono), contrasena)
 
-#lector.registrar_usuario(usuario)
+        if existe_usuario == False or telefono_valido == False:
+            alert_message(
+                screen,
+                50,
+                5,
+                "ATENCION",
+                "No existe ningun usuario con ese telefono y contrasena",
+                "ENTER",
+            )
+            continuar_verificacion = confirmation_box(
+                screen, "¿Quieres seguir intentando?"
+            )
+        else:
+            return (int(telefono), contrasena)
 
-print(lector.busqueda_recursiva_por_ocupacion(usuario, "carpintero"))
 
-lector.mostrar_tablas()
+def signup_page(screen):
+    nombre = None
+    apellido = None
+    telefono = None
+    contrasena = None
+    genero = None
+    edad = None
+    continuar_registro = True
+    hay_campos_invalidos = False
+    while continuar_registro == True:
+        telefono = textbox_builder(screen, 50, 10, "¿Cual es tu telefono?", "ENTER")
+        contrasena = textbox_builder(screen, 50, 10, "¿Cual es tu contrasena?", "ENTER")
+        nombre = textbox_builder(screen, 50, 10, "¿Cual es tu nombre?", "ENTER")
+        apellido = textbox_builder(
+            screen, 50, 10, "¿Cuales son tus apellidos?", "ENTER"
+        )
+        edad = textbox_builder(screen, 50, 10, "¿Cual es tu edad?", "ENTER")
+        genero = menu_builder(
+            screen,
+            50,
+            "¿Cual es tu genero?",
+            ["Hombre", "Mujer"],
+            "Presionar <Enter> para confirmar.",
+        )
+        genero = 0 if genero == "Hombre" else 1
+
+        if es_telefono_valido(telefono) == False:
+            hay_campos_invalidos = True
+        if edad.isnumeric() == False:
+            hay_campos_invalidos = True
+
+        if hay_campos_invalidos == True:
+            alert_message(
+                screen,
+                50,
+                5,
+                "ATENCION",
+                "Algunos campos que ingresaste no son validos...",
+                "ENTER",
+            )
+            continuar_registro = confirmation_box(screen, "¿Quieres seguir intentando?")
+        if hay_campos_invalidos == False:
+            usuario = Usuario(
+                telefono, contrasena, genero, edad, nombre, apellido, True, True
+            )
+            if ESCRITOR_DB.registrar_usuario(usuario) == False:
+                alert_message(
+                    screen,
+                    50,
+                    5,
+                    "ATENCION",
+                    "Parece que ya existe un usuario con ese telefono...",
+                    "ENTER",
+                )
+                continuar_registro = confirmation_box(
+                    screen, "¿Quieres volver a intentar?"
+                )
+            else:
+                alert_message(
+                    screen,
+                    50,
+                    5,
+                    "EXITOS!",
+                    "Tu usuario a sido registrado con exito, disfruta!",
+                    "ENTER",
+                )
+
+
+def main_page(screen, usuario: Usuario):
+    option = menu_builder(
+        screen,
+        50,
+        "¿Que quieres hacer?",
+        ["Buscar Trabajador", "Ver mi perfil"],
+        "Presiona <Enter> para continuar.",
+    )
+    if option == "Buscar Trabajador":
+        pass
+    elif option == "Ver mi perfil":
+        user_page(screen, usuario)
+
+
+def user_page(screen, usuario: Usuario):
+    while True:
+        option = menu_builder(
+            screen,
+            50,
+            "MI PERFIL",
+            [
+                "Ver mis datos",
+                "Agregar contacto",
+                "Eliminar contacto",
+                "Agregar ocupacion",
+                "Eliminar ocupacion",
+            ],
+            "Presionar <Enter> para continuar.",
+            ["Regresar"],
+        )
+        if option == "Ver mis datos":
+            pass
+        elif option == "Agregar contacto":
+            telefono_contacto = textbox_builder(
+                screen, 50, 10, "Ingresa el telefono de tu conocido:", "Enter"
+            )
+            if (
+                es_telefono_valido(telefono_contacto) == False
+                or ESCRITOR_DB.existe_contacto(usuario.TELEFONO, telefono_contacto)
+                == True
+            ):
+                alert_message(
+                    screen,
+                    50,
+                    5,
+                    "ATENCION",
+                    "Parece que el telefono ingresado no es valido, o ya esta en tu lista de contactos",
+                    "ENTER",
+                )
+            else:
+                ESCRITOR_DB.agregar_contacto(usuario, telefono_contacto)
+                alert_message(
+                    screen,
+                    50,
+                    5,
+                    "EXITO",
+                    "Tu contacto fue agregado con exito",
+                    "ENTER",
+                )
+
+        elif option == "Eliminar contacto":
+            pass
+        elif option == "Agregar ocupacion":
+            pass
+        elif option == "Eliminar ocupacion":
+            pass
+        elif option == "Salir":
+            break
+
+
+def search_page(screen):
+    pass
+
+
+def main(screen: curses.window):
+    # MAIN SETTINGS
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.curs_set(0)
+
+    print_centered(screen, banner, optional_message="Presionar cualquier tecla")
+    screen.getch()
+
+    # MAIN FLOW
+    while True:
+        screen.clear()
+        screen.refresh()
+        selected_option = menu_builder(
+            screen,
+            40,
+            "BIENVENIDO",
+            ["Ingresar", "Registrarme", "Sobre nosotros", "Estadisticas"],
+            "Presionar <Enter> para confirmar",
+            default_options=["Salir"],
+        )
+        screen.clear()
+
+        if selected_option == "Ingresar":
+            telefono_usuario, contrasena_usuario = login_page(screen)
+            usuario = ESCRITOR_DB.obtener_usuario(telefono_usuario, contrasena_usuario)
+            main_page(screen, usuario)
+        elif selected_option == "Registrarme":
+            signup_page(screen)
+        elif selected_option == "Sobre nosotros":
+            alert_message(
+                screen,
+                60,
+                20,
+                "SOBRE NOSOTROS",
+                r"""
+Hola! somos Chamba Ya! un programa para buscar trabajo, y dar trabajo, especialmente para trabajos mas practicos como la carpinterias o amas de cosas...
+                
+La aplicacion recomienda trabajadores basado en tus conocidos, y los conocidos de tus conocidos!
+                
+Disfruta!
+
+
+    Atentamente,
+    El equipo de ChambaYa!""",
+                "ENTER",
+            )
+        elif selected_option == "Estadisticas":
+            pass
+        elif selected_option == "Salir":
+            break
+
+
+curses.wrapper(main)
